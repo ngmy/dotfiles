373ee01683b4e7264101c51c2526fee6a8e7ab8b
Yuta Nagamiya <y.nagamiya@gmail.com>
2020-07-14 03:29:22 +0900

    Update the install script

--- install.sh
+++ install.sh
@@ -4,9 +4,9 @@ DOTFILES_PATH="${HOME}/dotfiles"
 
 do_it() {
   if [ -d "${DOTFILES_PATH}" ]; then
-    echo "dotfiles already exists in '${DOTFILES_PATH}'. Skip download."
+    echo "ngmy/dotfiles already exists in '${DOTFILES_PATH}'. Skip download."
   else
-    echo "Downloading dotfiles to '${DOTFILES_PATH}'..."
+    echo "Downloading ngmy/dotfiles to '${DOTFILES_PATH}'..."
     git clone https://github.com/ngmy/dotfiles.git "${DOTFILES_PATH}"
   fi
   find "${DOTFILES_PATH}" -name '.*' -not -name '.git' -mindepth 1 -maxdepth 1 -exec ln -nfsv {} "${HOME}" \;
