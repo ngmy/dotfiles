91ce750a7e6c0c02e55ba8436535a610ef2e7d55
Yuta Nagamiya <y.nagamiya@gmail.com>
2020-07-14 18:57:46 +0900

    Update the install script to get a backup before creating a symbolic link

--- install.sh
+++ install.sh
@@ -11,7 +11,11 @@ do_it() {
     git -C "${DOTFILES_PATH}" submodule init
     git -C "${DOTFILES_PATH}" submodule update
   fi
-  find "${DOTFILES_PATH}" -name '.*' -not -name '.git' -mindepth 1 -maxdepth 1 -exec ln -nfsv {} "${HOME}" \;
+  BACKUP_DATE="$(date +%Y%m%d_%H%M%S)"
+  find "${DOTFILES_PATH}" -name '.*' -not -name '.git' -mindepth 1 -maxdepth 1 -exec basename {} \; \
+    | xargs -n 1 find "${HOME}" -maxdepth 1 -not -type l -name \
+    | xargs -I {} mv -v {} "{}.${BACKUP_DATE}"
+  find "${DOTFILES_PATH}" -name '.*' -not -name '.git' -mindepth 1 -maxdepth 1 -exec ln -fnsv {} "${HOME}" \;
   vim +PluginInstall +qall
 }
 
